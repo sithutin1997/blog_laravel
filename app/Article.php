57c2d795e59c20526342f3bb2907c03b80f6c567
Si Thu Tin <sethu.tin@gmail.com>
2021-01-07 01:03:18 +0630

    relationship

--- app/Article.php
+++ app/Article.php
@@ -7,5 +7,13 @@ use Illuminate\Database\Eloquent\Model;
 class Article extends Model
 {
     //
+    public function category()
+    {
+        return $this->belongsTo('App\Category');
+    }
 
+    public function comment()
+    {
+        return $this->hasMany('App\Comment');
+    }
 }
--- database/migrations/2021_01_06_181552_create_categories_table.php
+++ database/migrations/2021_01_06_181552_create_categories_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateCommentsTable extends Migration
+class CreateCategoriesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,9 @@ class CreateCommentsTable extends Migration
      */
     public function up()
     {
-        Schema::create('comments', function (Blueprint $table) {
+        Schema::create('categories', function (Blueprint $table) {
             $table->id();
-            $table->text('content');
-            $table->integer('article_id');
+            $table->string('name');
             $table->timestamps();
         });
     }
@@ -28,6 +27,6 @@ class CreateCommentsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('comments');
+        Schema::dropIfExists('categories');
     }
 }
